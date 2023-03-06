aea1a5c6716cdebd8dc25a2657aed1865de01d90
DeepakJaiz <120568685+DeepakJaiz@users.noreply.github.com>
2023-03-06 16:28:01 +0530

    Update whatsappData.js

--- whatsappData.js
+++ whatsappData.js
@@ -1,42 +1,42 @@
 let chatData  = [
     {
        id:97, name:"Deepak Jaiswal",mobile:9304161106,password:"deepak123",img:"https://pps.whatsapp.net/v/t61.24694-24/324803036_576912044314467_1170769735205361919_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdSXx82tx_78-TCABy28Z4aTSswlXIh9fmFq_bQYE4tilg&oe=6402F952",contact:[
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
+            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
         ]
     },
     {
         id:100,name:"Rakesh Singh",mobile:9876543211,password:"rakesh123",img:"",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]}
+            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     },
     {
         id:101,name:"Amit Jaiswal",mobile:1234567890,password:"amit123",img:"https://tse1.mm.bing.net/th?id=OIP.XCDNOTmrvU_DKioT-1g_uQHaGl&pid=Api&P=0",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]}
+            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     },
     {
         id:102,name:"Satyam Anand",mobile:1122334455,password:"satyam123",img:"",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]}
+            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     },
     {
         id:103,name:"Aman Singh",mobile:9988776655,password:"aman123",img:"",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20"},{type:"rcv",msg:"Hello Rakesh",time:"1:22"},{type:"send",msg:"How are you",time:"1:24"}]}
+            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     }
     ]
