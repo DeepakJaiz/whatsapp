f0e9d880c259be0cd84265903644a7b0ece78c7d
DeepakJaiz <120568685+DeepakJaiz@users.noreply.github.com>
2023-03-17 12:44:30 +0530

    Update whatsappServer.js

--- whatsappServer.js
+++ whatsappServer.js
@@ -29,7 +29,8 @@ const http = app.listen(port,()=>console.log(`Node App Listening on port ${port}
 const io = require("socket.io")(http,
     {
     cors: {
-      origin: "https://abcder-de751.web.app",
+      origin: "https://dfgjgj-e0653.web.app",
+         methods: ["GET", "POST"]
     },
   });
 
