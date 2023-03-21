122d1cdfe4550256eb378418d3bd9177d9c849e9
DeepakJaiz <120568685+DeepakJaiz@users.noreply.github.com>
2023-03-21 20:10:18 +0530

    Update whatsappData.js

--- whatsappData.js
+++ whatsappData.js
@@ -1,42 +1,42 @@
 let chatData  = [
     {
-       id:97, name:"Deepak Jaiswal",mobile:9304161106,password:"deepak123",img:"https://pps.whatsapp.net/v/t61.24694-24/324803036_576912044314467_1170769735205361919_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdSXx82tx_78-TCABy28Z4aTSswlXIh9fmFq_bQYE4tilg&oe=6402F952",contact:[
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+       id:97, name:"Deepak Jaiswal",mobile:9304161106,password:"deepak123",img:"https://tse3.mm.bing.net/th?id=OIP.-iTwvnzvblr7qSQop_nkGgHaH5&pid=Api&P=0",contact:[
+            {name:"Rakesh",mobile:9876543211 ,img :"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890 ,img :"https://tse1.mm.bing.net/th?id=OIP.XCDNOTmrvU_DKioT-1g_uQHaGl&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,img:"https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b_4NDN6jtfcsA0tTbdHRyWrsbdkogDvnZQ&usqp=CAU",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
         ]
     },
     {
-        id:100,name:"Rakesh Singh",mobile:9876543211,password:"rakesh123",img:"",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
+        id:100,name:"Rakesh Singh",mobile:9876543211,password:"rakesh123",img :"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",contact:[
+            {name:"Deepak",mobile:9304161106,img:"https://tse3.mm.bing.net/th?id=OIP.-iTwvnzvblr7qSQop_nkGgHaH5&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,img :"https://tse1.mm.bing.net/th?id=OIP.XCDNOTmrvU_DKioT-1g_uQHaGl&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,img:"https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b_4NDN6jtfcsA0tTbdHRyWrsbdkogDvnZQ&usqp=CAU",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     },
     {
         id:101,name:"Amit Jaiswal",mobile:1234567890,password:"amit123",img:"https://tse1.mm.bing.net/th?id=OIP.XCDNOTmrvU_DKioT-1g_uQHaGl&pid=Api&P=0",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
+            {name:"Deepak",mobile:9304161106,img:"https://tse3.mm.bing.net/th?id=OIP.-iTwvnzvblr7qSQop_nkGgHaH5&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Rakesh",mobile:9876543211,img :"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,img:"https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b_4NDN6jtfcsA0tTbdHRyWrsbdkogDvnZQ&usqp=CAU",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     },
     {
-        id:102,name:"Satyam Anand",mobile:1122334455,password:"satyam123",img:"",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Aman",mobile:9988776655,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
+        id:102,name:"Satyam Anand",mobile:1122334455,password:"satyam123",img:"https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg",contact:[
+            {name:"Deepak",mobile:9304161106,img:"https://tse3.mm.bing.net/th?id=OIP.-iTwvnzvblr7qSQop_nkGgHaH5&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,img :"https://tse1.mm.bing.net/th?id=OIP.XCDNOTmrvU_DKioT-1g_uQHaGl&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Rakesh",mobile:9876543211,img :"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Aman",mobile:9988776655,img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b_4NDN6jtfcsA0tTbdHRyWrsbdkogDvnZQ&usqp=CAU",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     },
     {
-        id:103,name:"Aman Singh",mobile:9988776655,password:"aman123",img:"",contact:[
-            {name:"Deepak",mobile:9304161106,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Amit",mobile:1234567890,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Rakesh",mobile:9876543211,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
-            {name:"Satyam",mobile:1122334455,message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
+        id:103,name:"Aman Singh",mobile:9988776655,password:"aman123",img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3b_4NDN6jtfcsA0tTbdHRyWrsbdkogDvnZQ&usqp=CAU",contact:[
+            {name:"Deepak",mobile:9304161106,img:"https://tse3.mm.bing.net/th?id=OIP.-iTwvnzvblr7qSQop_nkGgHaH5&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Amit",mobile:1234567890,img :"https://tse1.mm.bing.net/th?id=OIP.XCDNOTmrvU_DKioT-1g_uQHaGl&pid=Api&P=0",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Rakesh",mobile:9876543211,img :"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]},
+            {name:"Satyam",mobile:1122334455,img:"https://www.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg",message:[{type:"send",msg:"Hii",time:"1:20",filetype:"text"},{type:"rcv",msg:"Hello Rakesh",time:"1:22",filetype:"text"},{type:"send",msg:"How are you",time:"1:24",filetype:"text"}]}
         ]
     }
     ]
